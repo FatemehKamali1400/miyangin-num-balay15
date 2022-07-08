108d3dd7b87b683088d4a12ada033e71eaf7b590
Yamahdi@1984 <fatemeh.kamali198422@gmail.com>
2022-07-09 02:28:55 +0430

    tamrin5

---
+++ miyangin num balay15/Program.cs
@@ -0,0 +1,22 @@
+﻿float num, m=0;
+int n=0;
+for (int i = 0; i < 10; i++)
+{
+    System.Console.WriteLine("please Enter num:");
+     num=float.Parse(Console.ReadLine());
+       ;
+       if (num>15)
+       {
+        m=m + num;
+        n++;
+       }  
+}
+System.Console.WriteLine("m= "+ m/n) ;
+
+
+ 
+ 
+
+  
+
+
